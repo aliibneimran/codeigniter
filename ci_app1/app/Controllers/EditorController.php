b9cfd1975cc72302b553a78161bed3a735d7c180
unknown <you@example.com>
2023-12-21 12:24:03 +0600

    add

--- ci_app1/app/Controllers/EditorController.php
+++ ci_app1/app/Controllers/EditorController.php
@@ -3,11 +3,42 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\CategoryModel;
+use App\Models\OrderModel;
+use App\Models\PropertyModel;
+use App\Models\ServiceModel;
+use App\Models\TypeModel;
 
 class EditorController extends BaseController
 {
+    protected $property = '';
+    protected $service = '';
+    protected $category = '';
+    protected $type = '';
+    protected $order = '';
+
+    protected $helpers = ['form', 'url', 'file'];
+    public function __construct()
+    {
+        $this->property = new PropertyModel();
+        $this->service = new ServiceModel();
+        $this->category = new CategoryModel();
+        $this->type = new TypeModel();
+        $this->order = new OrderModel();
+    }
     public function index()
     {
-        return view('dashboard');
+        $totalProperty = $this->property->countAllResults();
+        $totalService = $this->service->countAllResults();
+        $totalCategory = $this->category->countAllResults();
+        $totalType = $this->type->countAllResults();
+        $totalOrder = $this->order->countAllResults();
+
+        $data['totalProperty'] = $totalProperty;
+        $data['totalService'] = $totalService;
+        $data['totalCategory'] = $totalCategory;
+        $data['totalType'] = $totalType;
+        $data['totalOrder'] = $totalOrder;
+        return view('dashboard',$data);
     }
 }
