bc340d8dc4e37c615b1542906349764f7412d08c
unknown <you@example.com>
2023-12-21 12:38:35 +0600

    add

--- ci_app1/app/Views/includes/editor_sidebar.php
+++ ci_app1/app/Views/includes/editor_sidebar.php
@@ -16,8 +16,7 @@
       </a>
       <ul id="service" class="nav-content collapse" data-bs-parent="#sidebar-nav">
         <li><a href="<?= base_url('/editor/services') ?>"><i class="bi bi-circle"></i><span>All Services</span></a></li>
-        <li><a href="<?= base_url('/editor/services/add-service') ?>"><i class="bi bi-circle"></i><span>Add
-              Service</span></a></li>
+        <li><a href="<?= base_url('/editor/services/add-service') ?>"><i class="bi bi-circle"></i><span>Add vService</span></a></li>
       </ul>
     </li>
     <!--Property-->
@@ -28,8 +27,7 @@
       <ul id="property" class="nav-content collapse" data-bs-parent="#sidebar-nav">
         <li><a href="<?= base_url('/editor/properties') ?>"><i class="bi bi-circle"></i><span>All Property</span></a>
         </li>
-        <li><a href="<?= base_url('/editor/properties/add-property') ?>"><i class="bi bi-circle"></i><span>Add
-              Property</span></a></li>
+        <li><a href="<?= base_url('/editor/properties/add-property') ?>"><i class="bi bi-circle"></i><span>Add Property</span></a></li>
       </ul>
     </li>
     <!--Category-->
@@ -39,8 +37,7 @@
       </a>
       <ul id="category" class="nav-content collapse" data-bs-parent="#sidebar-nav">
         <li><a href="<?= base_url('/editor/categories') ?>"><i class="bi bi-circle"></i><span>All Categories</span></a></li>
-        <li><a href="<?= base_url('/editor/categories/add-category') ?>"><i class="bi bi-circle"></i><span>Add
-              Category</span></a></li>
+        <li><a href="<?= base_url('/editor/categories/add-category') ?>"><i class="bi bi-circle"></i><span>Add Category</span></a></li>
       </ul>
     </li>
 
--- ci_app1/app/Views/includes/header.php
+++ ci_app1/app/Views/includes/header.php
@@ -2,7 +2,7 @@
 
   <div class="d-flex align-items-center justify-content-between">
     <a href="/" class="logo d-flex align-items-center">
-      <img src="../assets/img/logo.png" alt="">
+      <img src="<?= base_url('assets/img/logo.png') ?>" alt="">
       <?php
       if (session()->get('user_role') == 'admin') {
         echo "<span class='d-none d-lg-block'>Admin</span>";
@@ -176,7 +176,7 @@
       <li class="nav-item dropdown pe-3">
 
         <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-          <img src="../assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
+          <img src="<?= base_url('assets/img/profile-img.jpg') ?>" alt="Profile" class="rounded-circle">
           <span class="d-none d-md-block dropdown-toggle ps-2">
             <?= session()->name; ?>
           </span>
